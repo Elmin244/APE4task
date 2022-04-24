40d614ec734fb797701fe528324b896c70ae6929
Elmin244 <99805924+Elmin244@users.noreply.github.com>
2022-04-24 10:52:07 +0400

    Update Program.cs

--- 24042022task/24042022task/Program.cs
+++ 24042022task/24042022task/Program.cs
@@ -4,23 +4,24 @@ namespace _24042022task
 {
     static class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
+
             string text = "exTensiON clASSi StaTiC OlMaLiDIR";
             text = text.Capitalize();
-            Console.WriteLine(text);
+            Console.WriteLine(text); 
+
         }
         public static string Capitalize(this string text)
         {
             text = text.ToLower();
+            StringBuilder sb = new StringBuilder();
             string[] promises = text.Split(' ');
-            string newtext = " ";
             foreach (var promise in promises)
             {
-                string newPromise = char.ToUpper(promise[0]) + promise.Substring(1) + "";
-                newtext += newPromise;
+                sb.Append(char.ToUpper(promise[0]) + promise.Substring(1) + " ");
             }
-            return text = newtext;
+            return text = sb.ToString();
 
         }
     }
