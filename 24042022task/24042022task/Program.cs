452438b912d6f08d25a1f121aa833e48d5bb10be
Elmin Huseyinli <tu7d5w0m5@aztu.local>
2022-04-24 10:14:45 +0400

    first commit

---
+++ 24042022task/24042022task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _24042022task
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "exTensiON clASSi StaTiC OlMaLiDIR";
+            text = text.Capitalize();
+            Console.WriteLine(text);
+        }
+        public static string Capitalize(this string text)
+        {
+            text = text.ToLower();
+            string[] promises = text.Split(' ');
+            string newtext = " ";
+            foreach (var promise in promises)
+            {
+                string newPromise = char.ToUpper(promise[0]) + promise.Substring(1) + "";
+                newtext += newPromise;
+            }
+            return text = newtext;
+
+        }
+    }
+}
