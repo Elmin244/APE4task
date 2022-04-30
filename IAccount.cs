526f815e94ee1924e91ca6a5ec964748874ff0b8
Elmin244 <99805924+Elmin244@users.noreply.github.com>
2022-04-30 10:19:27 +0400

    Add files via upload

---
+++ IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30042022Hometask
+{
+    interface IAccount
+    {
+        interface IAccount
+        {
+            bool PasswordChecker(string password);
+            void ShowInfo();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _30042022Hometask
+{
+    class Student
+    {
+        private static int _id;
+        public int Id { get; }
+        public string FullName { get; set; }
+        public double Point { get; set; }
+        private Student()
+        {
+            _id++;
+            Id = _id;
+        }
+        public Student(string fullName, double point) : this()
+        {
+            FullName = fullName;
+            Point = point;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($@"Id - {Id} Full Name -{FullName} Point - {Point}");
+        }
+
+    }
+}
